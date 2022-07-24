0d4de48e1aca23f0133ab064c093203fc82f7039
Anitor <anitorabraham@gmail.com>
2022-07-24 07:31:02 +0100

    worked on implementing config logic

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
-@Injectable()
+@Injectable() // we annotate classes with Injectable decorators to be able to use dependency injections.
 export class PrismaService extends PrismaClient {
-  constructor() {
+  constructor(config: ConfigService) {
     super({
       datasources: {
         db: {
-          url: 'postgresql://postgres:123@localhost:5434/nest?schema=public',
+          url: config.get('DATABASE_URL'),
         },
       },
     });
